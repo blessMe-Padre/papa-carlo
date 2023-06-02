27c02f44728176ba51a86ef270bf6de4715c2d66
Сергей Субботин <zarodiny@yandex.ru>
2023-06-02 16:16:08 +1000

    feat: добавляет анимацию

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,8 @@ import { initHeroSlider } from './modules/hero-slider';
 import { initNav } from './modules/nav';
 import { initScroll } from './modules/scroll';
 import { initScrollToTop } from './modules/scroll-to-top';
+import WOW from './modules/wow';
+
 
 window.addEventListener('DOMContentLoaded', () => {
   // Modules
@@ -9,4 +11,5 @@ window.addEventListener('DOMContentLoaded', () => {
   initHeroSlider();
   initScroll();
   initScrollToTop();
+  new WOW().init();
 });
